7ff26e3dcaa4dc78ce64c997797b7c94080415e9
bluesummerhotle <bluesummerhotle.hn@gmail.com>
2025-05-14 17:36:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,10 +49,11 @@ if uploaded_file:
             status_placeholder = st.empty()
 
             chrome_options = Options()
+            chrome_options.binary_location = "/usr/bin/chromium-browser"
             chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--window-size=1920,1080")
             chrome_options.add_argument("--user-agent=Mozilla/5.0")
 
